a87cfd0b5b7bb5dbe623650d3ddddb91581557a0
sunil Yadav <sunilyadav1998jan@gmail.com>
2025-07-15 00:31:25 +0530

    countrylistdata-add

--- Components/CountriesLists.jsx
+++ Components/CountriesLists.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import CountryCard from './CountryCard'
 import CountriesListShimmer from './CountriesListShimmer'
+import countriesData from '../CountriesData'
 
 export default function CountriesLists({ query }) {
-  const [countriesData, setCountriesData] = useState([])
+  // const [countriesData, setCountriesData] = useState([])
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then((res) => res.json())
-      .then((data) => {
-        setCountriesData(data)
-        // console.log(data[2].region);
-      })
-  }, [])
-
+  // useEffect(() => {
+  //   fetch('https://restcountries.com/v3.1/all')
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setCountriesData(data)
+  //     }).catch((err) => {
+  //       throw new Error(err)
+  //     }) 
+  // }, [])
+console.log(countriesData);
   return (
     <>
       {!countriesData.length ? (
